bc8526008cbe693ca38c5dc7d7a6240b6f440dac
Satya Saketh Bollepally <b.satyasaketh@gmail.com>
2026-08-15 11:21:50 +0530

    Add Java Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/27. Remove Element/Solution.java
+++ LeetCode/Patterns/Two Pointers/27. Remove Element/Solution.java
@@ -1,15 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int n = nums.length;
         int j = 0;
-        for(int i = 0; i<n; i++){
-            if(nums[i] != val){
-                int temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
-                j++;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[j++] = nums[i];
             }
         }
+
         return j;
     }
 }
