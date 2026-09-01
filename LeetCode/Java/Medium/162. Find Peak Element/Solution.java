635325cb3ce4df15a542e152593541ea42654ca5
Satya Saketh Bollepally <b.satyasaketh@gmail.com>
2026-09-01 16:50:47 +0530

    Add Medium Java Solution for 162. Find Peak Element from LeetCode

--- LeetCode/Java/Medium/162. Find Peak Element/Solution.java
+++ LeetCode/Java/Medium/162. Find Peak Element/Solution.java
@@ -7,7 +7,7 @@ class Solution {
             int mid = left + (right-left)/2;
 
             if(nums[mid] < nums[mid+1])
-                left = mid;
+                left = mid+1;
             else
                 right = mid;
         }
