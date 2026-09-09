4c4e23461cb9f70ef4535987e1a0c0c113f98e35
Satya Saketh Bollepally <b.satyasaketh@gmail.com>
2026-09-09 16:52:40 +0530

    Add Medium Java Solution for 198. House Robber from LeetCode

--- LeetCode/Java/Medium/198. House Robber/Solution.java
+++ LeetCode/Java/Medium/198. House Robber/Solution.java
@@ -1,16 +1,42 @@
 class Solution {
-    public int rob(int[] nums) {
-        return helper(nums, 0);
+    public int rob(int[] nums){
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+
+        return helper(nums, dp, 0);
     }
 
-    public int helper(int[] nums, int i){
+    public int helper(int[] nums, int[] dp, int i){
         if(i >= nums.length){
             return 0;
         }
 
-        int rob = nums[i] + helper(nums, i+2);
-        int skip = helper(nums, i+1);
+        if(dp[i] != -1){
+            return dp[i];
+        }
 
-        return Math.max(rob, skip);
+        int rob = nums[i] + helper(nums, dp, i+2);
+        int skip = helper(nums, dp, i+1);
+
+        dp[i] = Math.max(rob, skip);
+        return dp[i];
     }
 }
+
+// class Solution {
+//     public int rob(int[] nums) {
+//         return helper(nums, 0);
+//     }
+
+//     public int helper(int[] nums, int i) {
+
+//         if (i >= nums.length) {
+//             return 0;
+//         }
+
+//         int rob = nums[i] + helper(nums, i + 2);
+//         int skip = helper(nums, i + 1);
+
+//         return Math.max(rob, skip);
+//     }
+// }
