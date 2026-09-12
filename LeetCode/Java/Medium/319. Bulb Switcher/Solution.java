b4383f19c4d221d318c8ff0a89418c915b8a03f7
Satya Saketh Bollepally <b.satyasaketh@gmail.com>
2026-09-12 15:47:02 +0530

    Add Medium Java Solution for 319. Bulb Switcher from LeetCode

--- LeetCode/Java/Medium/319. Bulb Switcher/Solution.java
+++ LeetCode/Java/Medium/319. Bulb Switcher/Solution.java
@@ -4,22 +4,46 @@
 //     }
 // }
 
+// class Solution {
+//     public int bulbSwitch(int n) {
+
+//         boolean[] bulbs = new boolean[n + 1];
+
+//         for (int i = 1; i <= n; i++) {
+
+//             for (int j = i; j <= n; j += i) {
+//                 bulbs[j] = !bulbs[j];
+//             }
+//         }
+
+//         int count = 0;
+
+//         for (int i = 1; i <= n; i++) {
+//             if (bulbs[i]) {
+//                 count++;
+//             }
+//         }
+
+//         return count;
+//     }
+// }
+
 class Solution {
     public int bulbSwitch(int n) {
 
-        boolean[] bulbs = new boolean[n + 1];
+        int[] toggles = new int[n + 1];
 
         for (int i = 1; i <= n; i++) {
 
             for (int j = i; j <= n; j += i) {
-                bulbs[j] = !bulbs[j];
+                toggles[j]++;
             }
         }
 
         int count = 0;
 
         for (int i = 1; i <= n; i++) {
-            if (bulbs[i]) {
+            if (toggles[i] % 2 == 1) {
                 count++;
             }
         }
