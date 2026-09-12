037da08c51f48554d5f0728511f6c396675ca9dd
Satya Saketh Bollepally <b.satyasaketh@gmail.com>
2026-09-12 15:40:29 +0530

    Add Medium Java Solution for 319. Bulb Switcher from LeetCode

--- LeetCode/Java/Medium/319. Bulb Switcher/Solution.java
+++ LeetCode/Java/Medium/319. Bulb Switcher/Solution.java
@@ -1,9 +1,5 @@
 class Solution {
     public int bulbSwitch(int n) {
-        if(n == 0)
-            return 0;
-        if(n >= 3)
-            return 1;
         return (int)Math.sqrt(n);
     }
 }
